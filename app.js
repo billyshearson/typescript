9410e659d7ab1b3678fdafb97832aca26af6d84a
billyshears <63904750+billyshearson@users.noreply.github.com>
2020-08-07 07:54:20 +0900

    section3 done

--- app.js
+++
@@ -1,8 +0,0 @@
-var person = {
-    name: "yota",
-    age: 30,
-    hobbies: ["Sports", "Game"],
-    role: [1, "KILLING"]
-};
-console.log(person.age);
-var Takaki = { name: "Takaki", age: 5 };
--- app.ts
+++ app.ts
@@ -1,20 +1,5 @@
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string];
-} = {
-  name: "yota",
-  age: 30,
-  hobbies: ["Sports", "Game"],
-  role: [1, "KILLING"],
-};
+const button = document.querySelector("button")!;
 
-console.log(person.age);
-
-type People = {
-  name: string;
-  age: number;
-};
-
-const Takaki: People = { name: "Takaki", age: 5 };
+button.addEventListener("click", () => {
+  console.log("Clicked!");
+});
