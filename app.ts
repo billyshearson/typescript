50c44bfbcee43a80943e1b7cc2aba990abfdfdf6
billyshears <63904750+billyshearson@users.noreply.github.com>
2020-08-16 21:01:51 +0900

    section6 done but some are difficult thus need to reflect

--- app.ts
+++ app.ts
@@ -1,14 +1,75 @@
-class Department {
+type Admin = {
   name: string;
+  privileges: string[];
+};
 
-  constructor(n: string) {
-    this.name = n;
+type Employee = {
+  name: string;
+  startData: Date;
+};
+
+type ElevatedEmployee = Admin & Employee;
+
+const e1: ElevatedEmployee = {
+  name: "Max",
+  privileges: ["admin"],
+  startData: new Date(),
+};
+
+type Combinable = string | number;
+type Numeric = string | number;
+
+type Universal = Combinable & Numeric;
+
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
   }
+  return a + b;
+}
 
-  describe(this: Department) {
-    console.log("Department:" + this.name);
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+  console.log(emp.name);
+  if ("privileges" in emp) {
+    console.log("Privileges: " + emp.privileges);
+  }
+  if ("startData" in emp) {
+    console.log("StartDate: " + emp.startData);
   }
 }
 
-const accounting = new Department("Accounting");
-accounting.describe();
+printEmployeeInformation(e1);
+
+class Car {
+  drive() {
+    console.log("running");
+  }
+  loadCargo(amount: number) {
+    console.log("Having luggages " + amount);
+  }
+}
+
+interface Bird {
+  kind: "bird";
+  flyingSpeed: number;
+}
+
+interface Horse {
+  kind: "horse";
+  runningSpeed: number;
+}
+type Animal = Bird | Horse;
+
+function moveAnimal(animal: Animal) {
+  let speed: number;
+  switch (animal.kind) {
+    case "bird":
+      speed = animal.flyingSpeed;
+      break;
+    case "horse":
+      speed = animal.runningSpeed;
+      break;
+  }
+}
--- dist/app.js
+++ dist/app.js
@@ -1,12 +1,22 @@
 "use strict";
-var Department = /** @class */ (function () {
-    function Department(n) {
-        this.name = n;
+var e1 = {
+    name: "Max",
+    privileges: ["admin"],
+    startData: new Date(),
+};
+function add(a, b) {
+    if (typeof a === "string" || typeof b === "string") {
+        return a.toString() + b.toString();
     }
-    Department.prototype.describe = function () {
-        console.log("Department:" + this.name);
-    };
-    return Department;
-}());
-var accounting = new Department("Accounting");
-accounting.describe();
+    return a + b;
+}
+function printEmployeeInformation(emp) {
+    console.log(emp.name);
+    if ("privileges" in emp) {
+        console.log("Privileges: " + emp.privileges);
+    }
+    if ("startData" in emp) {
+        console.log("StartDate: " + emp.startData);
+    }
+}
+printEmployeeInformation(e1);
